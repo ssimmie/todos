1000274726ef8150e8197ed8e5e0ce82f46a2e9b
Sean Simmie <sean.simmie@gmail.com>
2019-02-02 17:32:55 +0000

    Initial project template

---
+++ src/main/java/net/ssimmie/todos/TodosApplication.java
@@ -0,0 +1,12 @@
+package net.ssimmie.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodosApplication.class, args);
+  }
+}
