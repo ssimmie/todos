872be8939e50e8c08e326cdc7ab9877a395b1d1d
ssimmie <ssimmie@users.noreply.github.com>
2020-03-04 11:17:46 +0000

    Test checkstyle
    Not to be merged

--- application/src/main/java/net/ssimmie/todos/TodosApplication.java
+++ application/src/main/java/net/ssimmie/todos/TodosApplication.java
@@ -1,6 +1,6 @@
 package net.ssimmie.todos;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
