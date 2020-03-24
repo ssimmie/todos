93aa7efbe60d14aa05bb7765363d4ab4f1db92e3
Sean Simmie <sean.simmie@gmail.com>
2020-03-24 16:20:53 +0000

    Bootstrap todos for performance benchmarking

--- performance-benchmark/src/test/scala/net/ssimmie/todos/performance/benchmark/BenchmarkSimulation.scala
+++ performance-benchmark/src/test/scala/net/ssimmie/todos/performance/benchmark/BenchmarkSimulation.scala
@@ -6,16 +6,12 @@ import io.gatling.http.Predef._
 class BenchmarkSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io")
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-    .doNotTrackHeader("1")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .acceptEncodingHeader("gzip, deflate")
-    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+    .baseUrl("http://localhost:8181")
+    .acceptHeader("application/json")
 
-  val scn = scenario("BasicSimulation")
-    .exec(http("request_1")
-      .get("/"))
+  val scn = scenario("Todos Actuator")
+    .exec(http("healthcheck")
+      .get("/actuator/health"))
     .pause(5)
 
   setUp(
