c1e2a79c220695e8f3f03554f22c5c1c87cef34a
Sean Simmie <sean.simmie@gmail.com>
2020-05-20 00:05:33 +0100

    Add root resource integration tests

--- application/src/main/java/net/ssimmie/todos/application/api/RootResource.java
+++ application/src/main/java/net/ssimmie/todos/application/api/RootResource.java
@@ -7,6 +7,7 @@ import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -15,6 +16,7 @@ class RootResource {
 
   @GetMapping
   Mono<RepresentationModel<?>> get() {
+    BlockHound.install();
     final Class<RootResource> rootResource = RootResource.class;
 
     return linkTo(methodOn(rootResource).get())
