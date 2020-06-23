c6a09aef5a353b1e205168d96409ec9d992c751d
Sean Simmie <sean.simmie@gmail.com>
2020-06-23 22:05:14 +0100

    Remove duplicate request for documentation

--- acceptance-tests/src/test/java/net/ssimmie/todos/DocumentationSteps.java
+++ acceptance-tests/src/test/java/net/ssimmie/todos/DocumentationSteps.java
@@ -1,6 +1,5 @@
 package net.ssimmie.todos;
 
-import static net.ssimmie.todos.BaseSteps.restClient;
 import static net.ssimmie.todos.BaseSteps.serverUri;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,22 +12,23 @@ import java.io.IOException;
 
 public class DocumentationSteps {
 
+  private HtmlPage page;
+
   @When("the api client requests the application's documentation site")
-  public void theApiClientRequestsTheApplicationSDocumentationSite() {
-    restClient.getForEntity("/docs/index.html", String.class);
+  public void theApiClientRequestsTheApplicationSDocumentationSite() throws IOException {
+    try (final WebClient webClient = new WebClient()) {
+      this.page = webClient.getPage(serverUri.resolve("/docs/index.html").toURL());
+    }
   }
 
   @Then("the api documentation content is returned")
   public void theApiDocumentationContentIsReturned() throws IOException {
-    try (final WebClient webClient = new WebClient()) {
-      final HtmlPage page = webClient.getPage(serverUri.resolve("/docs/index.html").toURL());
-      assertEquals("Todos Application API Documentation", page.getTitleText());
+    assertEquals("Todos Application API Documentation", page.getTitleText());
 
-      final String pageAsXml = page.asXml();
-      assertTrue(pageAsXml.contains("Todos Healthcheck Resource"));
-      assertTrue(pageAsXml.contains("{\"status\":\"UP\"}"));
-      assertTrue(pageAsXml.contains("Todos Root Resource"));
-      assertTrue(pageAsXml.contains("\"href\":\"http://localhost:8080/\""));
-    }
+    final String pageAsXml = page.asXml();
+    assertTrue(pageAsXml.contains("Todos Healthcheck Resource"));
+    assertTrue(pageAsXml.contains("{\"status\":\"UP\"}"));
+    assertTrue(pageAsXml.contains("Todos Root Resource"));
+    assertTrue(pageAsXml.contains("\"href\":\"http://localhost:8080/\""));
   }
 }
