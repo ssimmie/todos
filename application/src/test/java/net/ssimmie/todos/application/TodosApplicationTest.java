35a64d77d436482617661f04dcc54b8e64effb49
Sean Simmie <sean.simmie@gmail.com>
2021-01-23 21:58:23 +0000

    Simplify test

--- application/src/test/java/net/ssimmie/todos/application/TodosApplicationTest.java
+++ application/src/test/java/net/ssimmie/todos/application/TodosApplicationTest.java
@@ -2,7 +2,7 @@ package net.ssimmie.todos.application;
 
 import static net.ssimmie.todos.application.TodosApplication.main;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType.HAL;
 
 import org.junit.jupiter.api.Test;
@@ -24,10 +24,6 @@ class TodosApplicationTest {
 
   @Test
   public void shouldBeAbleToStartTheApplication() {
-    try {
-      main(new String[] {"--logging.level.root=ERROR", "--server.port=0"});
-    } catch (Exception e) {
-      fail("Application startup failed");
-    }
+    assertDoesNotThrow(() -> main(new String[] {"--server.port=0"}));
   }
 }
